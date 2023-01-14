6e819a43cb2cdeeb123b5b972b863d5db3506b5e
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-01-14 13:28:20 +0200

    Initial commit

---
+++ TwoSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(",",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int targetNum = int.Parse(Console.ReadLine());
+
+            Solution solutions = new Solution(numbers, targetNum);
+
+            int[] result = solutions.TwoSum(numbers, targetNum);
+            
+            Console.WriteLine($"[{ string.Join(",", result)}]");
+
+        }
+
+    }
+
+}
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Two_Sum
+{
+    public class Solution
+    {
+        public Solution(int[] nuumbers, int targetNum)
+        {
+            Nuumbers = new int[nuumbers.Length];
+            TargetNum = targetNum;            
+        }
+
+        public int[] Nuumbers { get; set; }
+        public int TargetNum { get; set; }        
+
+        public  int[] TwoSum(int[] nums, int targetNum)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length -1; i++)
+            {                             
+
+                int crrNum = nums[i];
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int secondNum = nums[j];
+                    if (crrNum + secondNum == targetNum)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
