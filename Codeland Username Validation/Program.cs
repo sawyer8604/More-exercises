9407067fdf5b06fedaf3fd5c5b08f1d7673f9727
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-02-03 16:56:30 +0200

    Initial commit

---
+++ Codeland Username Validation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Codeland_Username_Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // keep this function call here
+            Console.WriteLine(CodelandUsernameValidation(Console.ReadLine()));
+           
+        }
+        public static bool CodelandUsernameValidation(string str)
+        {
+
+
+            // code goes here  
+
+            if (!(str.Length >= 4 && str.Length <= 25))
+            {
+                return false;
+            }
+
+            if (!(char.IsLetter(str[0]) && str[str.Length - 1] != '_'))
+            {
+                return false;
+            }
+
+            foreach (var letter in str)
+            {
+                if (!(char.IsLetterOrDigit(letter) || (letter =='_')))
+                {
+
+                    return false;
+
+                }
+            }
+
+            return true;
+                        
+
+        }
+    }
+}
---
+++ Longest Word/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Longest_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestWord(Console.ReadLine()));
+        }
+
+        public static string LongestWord(string sen)
+        {
+            string result = string.Empty;
+            string longestWord = string.Empty;
+
+            for (int i = 0; i < sen.Length; i++)
+            {
+                if (char.IsLetterOrDigit(sen[i]))
+                {
+                    result += sen[i];
+
+                    if(longestWord.Length < result.Length)
+                    {
+                        longestWord = result;
+                    }
+                }
+                else
+                {
+                    result = string.Empty;
+                }
+
+            }
+            sen = longestWord;
+            return sen;
+
+        }
+    }
+
+}
