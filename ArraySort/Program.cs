b5db85e8c10e2329e3364832cde3698bc5f4a377
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-05-06 18:51:27 +0300

    Initial commit

---
+++ ArraySort/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ArraySort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 5, 7, 11, -1, 0, 72, 100, 1};
+
+            bool isSorted = false;
+
+            // ascending Sort
+            while (!isSorted)
+            {
+                for (int i = 0; i < numbers.Length -1; i++)
+                {
+                    if (numbers[i] > numbers[i+1])
+                    {
+                        int crrElement = numbers[i];
+                        numbers[i] = numbers[i+1];
+                        numbers[i+1] = crrElement;
+                        
+                    }
+
+                }
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] <= numbers[j + 1])
+                    {
+                        isSorted = true;
+                    }
+                    else
+                    {
+                        isSorted = false;
+                        break;
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            // Descending Sort.
+
+            Console.WriteLine(new string('=', 15));
+
+            int[] numberss = new int[] { 1, 5, 7, 11, -1, 0, 72, 100, 1 };
+
+            bool isSortedDescending = false;
+            
+            while (!isSortedDescending)
+            {
+                for (int i = 0; i < numberss.Length - 1; i++)
+                {
+                    if (numberss[i] < numberss[i + 1])
+                    {
+                        int crrElement = numberss[i];
+                        numberss[i] = numberss[i + 1];
+                        numberss[i + 1] = crrElement;
+
+                    }
+
+                }
+                for (int j = 0; j < numberss.Length - 1; j++)
+                {
+                    int crEl = numberss[j];
+                    int crNexEl = numberss[j+1];
+                    if (numberss[j] >= numberss[j + 1])
+                    {
+                        isSortedDescending = true;
+                    }
+                    else
+                    {
+                        isSortedDescending = false;
+                        break;
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < numberss.Length; i++)
+            {
+                Console.WriteLine(numberss[i]);
+            }
+
+
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(1);
+            list.AddLast(2);
+            list.AddLast(3);                
+            list.AddLast(4);
+            list.AddLast(5);
+           
+            
+            // remove element 
+            // count from 1
+
+            int indexToRemove = 4;
+
+            int counter = indexToRemove;
+
+            Stack<int> stack = new Stack<int>();
+
+            while (counter != 0)
+            {
+                foreach (var item in list)
+                {
+                    stack.Push(item);
+                    list.RemoveFirst();
+                    counter--;
+                    break;
+
+                }
+            }
+            
+            stack.Pop();
+            for (int i = 0; i < indexToRemove - 1; i++)
+            {
+                list.AddFirst(stack.Pop());
+            }
+                       
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
