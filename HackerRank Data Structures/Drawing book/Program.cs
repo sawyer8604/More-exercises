581d698ee3cbe8528eeb04228919c0ad31055ce5
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-05-06 13:26:31 +0300

    Initial commits

---
+++ HackerRank Data Structures/Drawing book/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p)
+    {
+        int[] sums = new int[2];
+
+        sums[0] = p / 2;
+        sums[1] = n / 2 - p / 2;
+
+        if (sums[0] < sums[1])
+        {
+            return sums[0];
+        }
+        else
+        {
+            return sums[1];
+        }
+        
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.pageCount(n, p);
+
+        Console.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ HackerRank Data Structures/Electronics Shop/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b)
+    {
+        int maxPrice = 0;
+        bool canBuy = false;
+
+        for (int i = 0; i < keyboards.Length; i++)
+        {
+            int crrKeyboardPrice = keyboards[i];
+
+            for (int j = 0; j < drives.Length; j++)
+            {
+                int crrDriverPrice = drives[j];
+
+                if(crrKeyboardPrice + crrDriverPrice <= b && crrKeyboardPrice + crrDriverPrice > maxPrice)
+                {
+                    maxPrice = crrKeyboardPrice + crrDriverPrice;
+                    canBuy = true;
+                }               
+
+            }
+        }
+        if (!canBuy)
+        {
+            return -1;
+        }
+
+        return maxPrice;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] bnm = Console.ReadLine().Split(' ');
+
+        int b = Convert.ToInt32(bnm[0]);
+
+        int n = Convert.ToInt32(bnm[1]);
+
+        int m = Convert.ToInt32(bnm[2]);
+
+        int[] keyboards = Array.ConvertAll(Console.ReadLine().Split(' '), keyboardsTemp => Convert.ToInt32(keyboardsTemp))
+        ;
+
+        int[] drives = Array.ConvertAll(Console.ReadLine().Split(' '), drivesTemp => Convert.ToInt32(drivesTemp))
+        ;
+        /*
+         * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+         */
+
+        int moneySpent = getMoneySpent(keyboards, drives, b);
+
+        Console.WriteLine(moneySpent);
+
+        //textWriter.Flush();
+       //textWriter.Close();
+    }
+}
